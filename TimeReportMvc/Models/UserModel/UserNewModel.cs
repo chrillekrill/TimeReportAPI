afe3efde25a510c20792afd736a598a784584df2
Christoffer K <henricson21@hotmail.com>
2022-05-19 09:46:27 +0200

    can now delete customers and projects. Can also create users

---
+++ TimeReportMvc/Models/UserModel/UserNewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TimeReportMvc.Models.UserModel;
+
+public class UserNewModel
+{
+    public string Username { get; set; }
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    public string Role { get; set; }
+    
+    public List<SelectListItem> AllRoles { get; set; } = new();
+
+    public UserNewModel()
+    {
+        List<string> roles = new List<string>()
+        {
+            "Admin",
+            "User"
+        };
+
+        AllRoles = roles.Select(role => new SelectListItem
+        {
+            Value = role,
+            Text = role
+        }).ToList();
+        AllRoles.Insert(0, new SelectListItem
+        {
+            Value = "",
+            Text = "Select a role"
+        });
+    }
+}
