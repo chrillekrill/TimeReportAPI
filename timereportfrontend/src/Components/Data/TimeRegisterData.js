d15cfbb9b4188eb7c5e9247a8eba971e9562acf4
Christoffer K <henricson21@hotmail.com>
2022-05-19 11:46:03 +0200

    Can now post time reports

--- timereportfrontend/src/Components/Data/TimeRegisterData.js
+++ timereportfrontend/src/Components/Data/TimeRegisterData.js
@@ -1,7 +1,51 @@
-const customerUrl = 'https://localhost:8080/customer'
-const projectUrl = 'https://localhost:8080/Project'
+import Cookies from 'universal-cookie';
 
-export const fetchProjects = () => {
+const cookies = new Cookies();
 
+const jwt = cookies.get('usertoken');
+const fetchOptions = {
+    method: 'GET',
+    headers: { 
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer " +  jwt,
+        'Access-Control-Allow-Origin': '*'
+    }
+}
+
+export const fetchProjects = async (id) => {  
+    const projectUrl = 'https://localhost:8080/Project/customer/' + id
+    
+    const response = await fetch(projectUrl, fetchOptions)  
+    const json = await response.json()
+
+    return json
+}
+
+export const fetchCustomers = async () => {
+    const customerUrl = 'https://localhost:8080/customer'
+    const response = await fetch(customerUrl, fetchOptions)  
+    const json = await response.json()
+
+    return json
+}
+
+export const postTimeRegister = async (data) => {
+    const registerUrl = 'https://localhost:8080/timereport'
+    const fetchOptionsPost = {
+        method: 'POST',
+        headers: { 
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer " +  jwt,
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(data)
+    }
+
+    const response = await fetch(registerUrl, fetchOptionsPost) 
+    const json = await response.status;
+    
+    return(json)
 }
 
